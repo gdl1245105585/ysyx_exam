014e7ead48fdbc538f53b6230459b96506a97f04
gdl1245105585 <20205487@stu.neu.edu.cn>
2022-11-17 02:06:01 -0800

    delate npc build

--- npc/build/obj_dir/Vmy_cpu__ConstPool_0.d
+++
@@ -1,4 +0,0 @@
-Vmy_cpu__ConstPool_0.o: Vmy_cpu__ConstPool_0.cpp \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h
